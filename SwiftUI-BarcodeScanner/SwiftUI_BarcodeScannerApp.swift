03f4a2cafd55cd1553cf9248d6c44de4468e59e5
drestwn <andremakmursetaiwan@gmail.com>
2023-11-16 09:50:13 +0700

    Initial Commit

---
+++ SwiftUI-BarcodeScanner/SwiftUI_BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BarcodeScannerApp.swift
+//  SwiftUI-BarcodeScanner
+//
+//  Created by Andre on 16/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
